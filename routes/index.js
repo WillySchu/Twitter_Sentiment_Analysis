0ab33f4fa617f499eadf954f04803382d2eeb955
Will Schumacher <meintodengel@gmail.com>
2016-04-18 17:33:50 -0700

    implement search function with twitter api

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const sentiment = require('sentiment');
+const searchTwitter = require('../lib/search');
 
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
 router.post('/', (req, res, next) => {
+  searchTwitter(req.body.test, () => {
+
+  });
   sentiment(req.body.test, (err, result) => {
     res.render('index', {test: result.score})
-  })
-})
+  });
+});
 
 module.exports = router;
