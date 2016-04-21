018e8d536969442e31340b4b7a94a25fdb3c99fa
Will Schumacher <meintodengel@gmail.com>
2016-04-21 15:56:33 -0700

    finish api route for single search

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,14 @@
 const express = require('express');
 const router = express.Router();
 
+function Searches() {
+  return knex('searches');
+}
+
 router.get('/', (req, res, next) => {
-  
-})
+  Searches().where({id: req.query.searchId}).first().then(search => {
+    res.send(search);
+  });
+});
 
 module.exports = router;
