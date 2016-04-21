9f3e378a440c0a6f3f029b8c213ab41e150b0d77
Will Schumacher <meintodengel@gmail.com>
2016-04-21 11:49:23 -0700

    resized buffer div for search pages

--- routes/searches.js
+++ routes/searches.js
@@ -15,33 +15,17 @@ function Boards() {
   return knex('boards');
 }
 
-function pSentiment(text) {
-  const promise = new Promise((resolve, reject) => {
-    sentiment(text, (err, result) => {
-      if (err) reject(err);
-      resolve(result);
-    });
-  });
-  return promise;
-}
-
 router.get('/new', (req, res, next) => {
   res.render('searches/new');
 });
 
 router.post('/', (req, res, next) => {
-  // // this route will just produce a result,
-  // // but will not insert anything into the tables
-  // searchTwitter(req.body.keyword, (tweets) => {
-  //   Promise.all(tweets).then(data => {
-  //     const scores = [];
-  //     for (var i = 0; i < data.length; i++) {
-  //       scores.push(data[i].score);
-  //     }
-  //     console.log(scores);
-      res.render('results', {scores: '(this is the scores object)'});
-  //   });
-  // });
+  // this route will just produce a result,
+  // but will not insert anything into the tables
+  searchTwitter(req.body.keyword, 10, (tweets) => {
+    console.log(tweets);
+    res.render('results', {scores: '(this is the scores object)'});
+  });
 });
 
 router.get('/new/:id', (req, res, next) => {
@@ -53,17 +37,17 @@ router.get('/new/:id', (req, res, next) => {
 });
 
 router.post('/:id', (req, res, next) => {
-  // // this route will produce the result page AND insert into the tables
-  // searchTwitter(req.body.keyword, (tweets) => {
-  //   Promise.all([tweets]).then(data => {
-  //     const scores = [];
-  //     for (var i = 0; i < data.length; i++) {
-  //       scores.push(data[i].score);
-  //     }
-  //     console.log(scores);
+  // this route will produce the result page AND insert into the tables
+  searchTwitter(req.body.keyword, (tweets) => {
+    Promise.all([tweets]).then(data => {
+      const scores = [];
+      for (var i = 0; i < data.length; i++) {
+        scores.push(data[i].score);
+      }
+      console.log(scores);
       res.render('results', {scores: '(this is the scores object)'});
-  //   });
-  // });
+    });
+  });
 });
 
 module.exports = router;
