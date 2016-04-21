a46103b939da9b87d5e248260435af8fbeb82c16
Will Schumacher <meintodengel@gmail.com>
2016-04-21 15:01:01 -0700

    re enable oauth

--- app.js
+++ app.js
@@ -32,20 +32,20 @@ app.use(cookieSession({
   keys: [process.env.SESSION_KEY]
 }));
 
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(passport.initialize());
-// app.use(passport.session());
-//
-// passport.use(new GithubStrategy ({
-//   clientID: process.env.GITHUB_CLIENT_ID,
-//   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-//   callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
-// },
-//   (accessToken, refreshToken, profile, done) => {
-//     User.findOrCreate({ githubid: profile.id }, (err, user) => {
-//       return done(err, user);
-//   });
-// }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GithubStrategy ({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    User.findOrCreate({ githubid: profile.id }, (err, user) => {
+      return done(err, user);
+  });
+}));
 
 passport.serializeUser((user, done) => {
   done(null, user);
