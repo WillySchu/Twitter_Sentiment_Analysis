9b54a3e5f8c2932e86e85068e6527a8c717e451d
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-04-22 10:08:28 -0700

    add new board routes

--- routes/searches.js
+++ routes/searches.js
@@ -62,11 +62,49 @@ router.get('/results', (req, res, next) => {
   }
 });
 
-// router.post('/results', (req, res, next) => {
-//   Boards_searches().insert({board_id: parseInt(req.body.boardId), search_id: parseInt(id)}).returning('board_id').then(boardId => {
-//     res.redirect('searches/results?searchId=' + id[0] + '&boardId=' + boardId);
-//   });
-// });
+router.post('/results', (req, res, next) => {
+  if (!req.body.boardname) {
+    if (req.user) {
+      Searches().first().orderBy('id', 'desc').then(search => {
+        Boards().first().where({name: req.body.board}).then(board => {
+          Boards_searches().insert({board_id: board.id, search_id: parseInt(search.id)}).returning('board_id').then(boardId => {
+            Boards()
+            .where('boards.id', boardId[0])
+            .join('boards_searches', 'boards_searches.board_id', '=', 'boards.id')
+            .join('searches', 'searches.id', '=', 'boards_searches.search_id')
+            .then(data => {
+              res.render('boards/show', {data});
+            });
+          });
+        })
+      });
+    } else {
+      res.redirect('login')
+    }
+  } else {
+    if (req.user) {
+      console.log(req.user);
+      Searches().first().orderBy('id', 'desc').then(search => {
+        console.log(search);
+        Boards().insert({user_id: req.user[0].id, name: req.body.boardname}).returning('*').then(board => {
+          console.log(board);
+          Boards_searches().insert({board_id: board[0].id, search_id: parseInt(search.id)}).returning('board_id').then(boardId => {
+            console.log(boardId);
+            Boards()
+            .where('boards.id', boardId[0])
+            .join('boards_searches', 'boards_searches.board_id', '=', 'boards.id')
+            .join('searches', 'searches.id', '=', 'boards_searches.search_id')
+            .then(data => {
+              res.render('boards/show', {data});
+            });
+          });
+        })
+      });
+    } else {
+      res.redirect('login')
+    }
+  }
+});
 
 router.get('/new/:id', (req, res, next) => {
   Boards().first()
