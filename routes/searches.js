c68ed060a49309936666696685965bff7015a6d4
Will Schumacher <meintodengel@gmail.com>
2016-04-22 11:47:46 -0700

    fix broken route

--- routes/searches.js
+++ routes/searches.js
@@ -73,7 +73,7 @@ router.post('/results', (req, res, next) => {
             .join('boards_searches', 'boards_searches.board_id', '=', 'boards.id')
             .join('searches', 'searches.id', '=', 'boards_searches.search_id')
             .then(data => {
-              res.render('/');
+              res.redirect('/');
             });
           });
         })
@@ -95,7 +95,7 @@ router.post('/results', (req, res, next) => {
             .join('boards_searches', 'boards_searches.board_id', '=', 'boards.id')
             .join('searches', 'searches.id', '=', 'boards_searches.search_id')
             .then(data => {
-              res.render('/');
+              res.redirect('/');
             });
           });
         })
