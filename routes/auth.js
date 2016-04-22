c71c53b07b8badc71b95def8d548ea4c82fffc8f
Spencer Scarpelli <spencerscarpelli@gmail.com>
2016-04-22 11:37:52 -0700

    add form page

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,41 @@ const router = express.Router();
 const unirest = require('unirest');
 const passport = require('passport');
 
+function createUser(data, callback) {
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) {
+      callback(err);
+    }
+
+    bcrypt.hash(data.password, salt, (err, hash) => {
+      if (err) {
+        callback(err);
+      }
+
+      data.password_digest = hash;
+      delete data.password;
+      Users().insert(data, '*').then((data) => {
+        callback(undefined, data);
+      });
+    });
+  });
+}
+
+function authenticateUser(email, password, callback) {
+  Users().where({email: email}).first().then(user => {
+    if (!user) {
+      return callback("Email and password don't match");
+    }
+    bcrypt.compare(password, user.password_digest, (err, isMatch) => {
+      if (err || !isMatch) {
+        return callback("Email and password don't match");
+      } else {
+        return callback(undefined, user);
+      }
+    });
+  });
+}
+
 router.get('/github',
   passport.authenticate('github', { scope: ['user:email'] })
 );
@@ -23,6 +58,17 @@ router.get('/logout', (req, res) => {
 
 router.get('/login', (req, res, next) => {
   res.render('login');
+});
+
+router.post('/signin', (req, res, next) => {
+  Users.authenticateUser(req.body.email, req.body.password, (err, user) => {
+    if (err) {
+      res.render('login', {error: err});
+    } else {
+      req.session.user = user;
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
