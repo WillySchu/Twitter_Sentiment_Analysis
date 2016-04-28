d85ddf73247a977dee6df7c441ee77b2d5d60d64
Will Schumacher <meintodengel@gmail.com>
2016-04-28 16:32:58 -0700

    refactor python sentiment analysis into seperate app

--- lib/format.py
+++
@@ -1,5 +0,0 @@
-s = ': [inv]'
-with open('inv.yml', 'w') as out_file:
-    with open('test.yml', 'r') as in_file:
-        for line in in_file:
-            out_file.write(line.rstrip('\n') + s + '\n')
